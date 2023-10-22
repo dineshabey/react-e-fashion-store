128ec2a0128b8ee30afa95139b3521e91efd28c7
Dinesh <dinabeysinghe@gmail.com>
2023-10-22 19:07:20 +0530

    products list image size design done

--- src/components/productList/singaleProduct/ProductSize.js
+++ src/components/productList/singaleProduct/ProductSize.js
@@ -1,7 +1,27 @@
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
+
+const defaultSize = ['XS', 'S', 'M', 'L', 'XL'];
 
 export default function ProductSize() {
   return (
-    <div>ProductSize</div>
+    <Grid container spacing={1}>
+      <Grid item xs={1}></Grid>
+      {defaultSize.map((val, key) => {
+        return <Grid item key={key} xs={2} >
+          <div style={{
+            border: '2px solid teal',
+            borderRadius: '7px',
+            backgroundColor: 'black',
+            color: 'white',
+          }}>
+            <Typography >
+              {val}
+            </Typography>
+          </div>
+        </Grid>
+      })}
+      <Grid item xs={1}></Grid>
+    </Grid>
   )
 }
