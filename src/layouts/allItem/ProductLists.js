0c5800c1579be0dd0c55bfc48f44660d09b99c8d
Dinesh <dinabeysinghe@gmail.com>
2023-10-22 12:42:05 +0530

    product mapping done

--- src/layouts/allItem/ProductLists.js
+++ src/layouts/allItem/ProductLists.js
@@ -1,30 +1,20 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 
+const products = [
+    1, 2, 3, 4,5,6,7,8,9,10
+];
+
 export default function ProductLists() {
     return (
         <Grid container>
-            <Grid item xs={3}>
-                <div style={{ backgroundColor: 'skyblue' }}>
-                    <h2>Product</h2>
-                </div>
-            </Grid>
-            <Grid item xs={3}>
-                <div style={{ backgroundColor: 'yellow' }}>
-                    <h2>Product</h2>
-                </div>
-            </Grid>
-            <Grid item xs={3}>
-                <div style={{ backgroundColor: 'red' }}>
-                    <h2>Product</h2>
-                </div>
-            </Grid>
-            <Grid item xs={3}>
-                <div style={{ backgroundColor: 'orange' }}>
-                    <h2>Product</h2>
-                </div>
-            </Grid>
-
+            {products.map((val, key) => {
+                return <Grid key={key} item xs={3}>
+                    <div style={{ backgroundColor: key % 2 === 0 ? 'orange': 'skyblue' }}>
+                        <h2>Product {val}</h2>
+                    </div>
+                </Grid>
+            })}
         </Grid>
     )
 }
