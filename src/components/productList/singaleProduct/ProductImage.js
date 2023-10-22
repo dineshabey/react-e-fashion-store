877e6b5120b31e2908fe8c402e023ed7fc3d27b3
Dinesh <dinabeysinghe@gmail.com>
2023-10-22 14:50:33 +0530

    products list image - OnMouseOver image change done

--- src/components/productList/singaleProduct/ProductImage.js
+++ src/components/productList/singaleProduct/ProductImage.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const images = [
+  'https://radikadilanka.com/rdfashion/static/media/product18.8cbfe59a.jpg',
+  'https://radikadilanka.com/rdfashion/static/media/product118.8609b05b.jpg'
+]
 
 export default function ProductImage() {
+  const [ImageSrc, SetimageSrc] = useState(images[0]);
   return (
-    <div style={{width:'100%',textAlign:'center'}}>
-      <img alt='productImage' src='https://radikadilanka.com/rdfashion/static/media/product11.91a23151.jpg'
+    <div style={{ width: '100%', textAlign: 'center' }}>
+      <img alt='productImage' src={ImageSrc}
         style={{
           width: '100%',
           height: 'auto',
-          borderRadius:'5px'
-        }}></img>
+          borderRadius: '5px'
+        }}
+        onMouseOver={() => {
+          SetimageSrc(images[1])
+        }}
+        onMouseOut={() => {
+          SetimageSrc(images[0])
+
+        }}
+      ></img>
 
     </div>
   )
