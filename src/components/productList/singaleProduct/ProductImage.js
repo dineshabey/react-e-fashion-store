0b764221993e414eb978e6ccf212fbfafac6ab15
Dinesh <dinabeysinghe@gmail.com>
2023-10-22 14:19:47 +0530

    products list image styles part done

--- src/components/productList/singaleProduct/ProductImage.js
+++ src/components/productList/singaleProduct/ProductImage.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function ProductImage() {
   return (
-    <div>ProductImage</div>
+    <div style={{width:'100%',textAlign:'center'}}>
+      <img alt='productImage' src='https://radikadilanka.com/rdfashion/static/media/product11.91a23151.jpg'
+        style={{
+          width: '100%',
+          height: 'auto',
+          borderRadius:'5px'
+        }}></img>
+
+    </div>
   )
 }
