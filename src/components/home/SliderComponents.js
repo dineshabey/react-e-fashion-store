5aa400584e844b6ac7cb574aaa75f4a1f610ca27
Dinesh <dinabeysinghe@gmail.com>
2023-10-22 08:28:05 +0530

    SHOP NOW buton isVisibale part done

--- src/components/home/SliderComponents.js
+++ src/components/home/SliderComponents.js
@@ -1,30 +1,32 @@
 import { Button } from '@mui/material'
 import React from 'react'
 
-export default function SliderComponents({ image }) {
+export default function SliderComponents({ image, isButtonVisibale }) {
     return (
         <div style={{ position: 'relative' }}>
-            <div style={{ position: 'absolute', width: '100%',height:'80vh',display:'flex' }}>
-                <div style={{margin:'0 auto',marginTop:'20%'}}>
+            {isButtonVisibale &&
+                <div style={{ position: 'absolute', width: '100%', height: '80vh', display: 'flex' }}>
+                    <div style={{ margin: '0 auto', marginTop: '20%' }}>
 
-                    <Button
-                        style={{
-                            color: 'white',
-                            borderRadius: '20px',
-                            border: '1px solid white',
-                            fontSize: '30px',
-                            padding: '5px 20px',
-                        }}
-                        sx={{
-                            '&:hover': {
-                                bgcolor: 'black',
-                            },
-                        }}
-                        variyant='outlined'
-                    >
-                        Shop now</Button>
+                        <Button
+                            style={{
+                                color: 'white',
+                                borderRadius: '20px',
+                                border: '1px solid white',
+                                fontSize: '30px',
+                                padding: '5px 20px',
+                            }}
+                            sx={{
+                                '&:hover': {
+                                    bgcolor: 'black',
+                                },
+                            }}
+                            variyant='outlined'
+                        >
+                            Shop now</Button>
+                    </div>
                 </div>
-            </div>
+            }
             <img src={image} alt='sliderImage' width='100%'></img>
         </div>
 
--- src/layouts/home/CarouselLayouts.js
+++ src/layouts/home/CarouselLayouts.js
@@ -8,25 +8,28 @@ export default function CarouselLayouts() {
         'https://radikadilanka.com/rdfashion/static/media/Yellow%20and%20White%20Fashion%20Facebook%20Cover2.7d37715a.jpg',
         'https://radikadilanka.com/rdfashion/static/media/Yellow%20and%20White%20Fashion%20Facebook%20Cover.1e91a4ae.jpg',
         'https://radikadilanka.com/rdfashion/static/media/Yellow_and_White_Fashion_Facebook_Cover3.3d57c154.jpg',
+        'https://radikadilanka.com/rdfashion/static/media/carasoule5.81c46b93.jpg',
+        'https://radikadilanka.com/rdfashion/static/media/carasoule6.f497aa12.jpg',
+        'https://radikadilanka.com/rdfashion/static/media/carasoule7.4adf9f2b.jpg',
     ]
 
     return (
-    
-            <CarouselProvider
-                naturalSlideWidth={100}
-                naturalSlideHeight={26}
-                totalSlides={3}
-            >
-                <Slider>
 
-                    {imageList.map((val, key) => {
-                        return <Slide key={key} index={key}>
-                            <SliderComponents image={val} />
-                        </Slide>
+        <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={26}
+            totalSlides={6}
+        >
+            <Slider>
 
-                    })}
-                </Slider>
-            </CarouselProvider>
-      
+                {imageList.map((val, key) => {
+                    return <Slide key={key} index={key}>
+                        <SliderComponents image={val} isButtonVisibale={key % 2 === 0} />
+                    </Slide>
+
+                })}
+            </Slider>
+        </CarouselProvider>
+
     )
 }
