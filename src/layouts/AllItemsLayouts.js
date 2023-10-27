6c4d6f4978cc7fb1b969dee743a5073389579a4a
Dinesh <dinabeysinghe@gmail.com>
2023-10-27 13:43:13 +0530

    data featching done

--- src/layouts/AllItemsLayouts.js
+++ src/layouts/AllItemsLayouts.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HeaderLayouts from './common/HeaderLayouts'
 import FotterLayouts from './common/FotterLayouts'
 import { Grid } from '@mui/material'
 import ProductsFilters from './allItem/ProductsFilters'
 import ProductLists from './allItem/ProductLists'
+import axios from 'axios'
+
+
+
+
 
 export default function AllItemsLayouts() {
+
+    useEffect(() => { //componets did mount
+        axios.get('https://cdn.radikadilanka.com:9000/getProducts').then((response) => {
+            console.log(response.data);
+        }).catch((e) => {
+            console.log(e)
+        })
+    }, [])
+
     return (
         <div>
             <HeaderLayouts />
