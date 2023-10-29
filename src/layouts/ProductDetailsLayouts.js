89f6dbdf6cf4e8f97f252fa16be2b774a7d46563
Dinesh <dinabeysinghe@gmail.com>
2023-10-29 18:26:51 +0530

    created ProductDetailsLayouts

---
+++ src/layouts/ProductDetailsLayouts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CommonWrapperLayouts from './common/CommonWrapperLayouts'
+
+export default function ProductDetailsLayouts() {
+    return (
+        <CommonWrapperLayouts>
+            <box>
+
+            </box>
+        </CommonWrapperLayouts>
+    )
+}
