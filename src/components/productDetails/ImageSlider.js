895749e21137ccdb9ca6a0c9ccdcc84e295867f6
Dinesh <dinabeysinghe@gmail.com>
2023-11-02 14:59:44 +0530

    add image slider for detailsLayouts

--- src/components/productDetails/ImageSlider.js
+++ src/components/productDetails/ImageSlider.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 export default function ImageSlider() {
   return (
-    <div>ImageSlider</div>
+    <Swiper
+      modules={[Pagination]}
+      pagination={{ clickable: true }}
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+
+    </Swiper>
   )
 }
--- src/layouts/productsDetails/productDetailsSections/qtyAndPriceSection/QtySelectSection.js
+++ src/layouts/productsDetails/productDetailsSections/qtyAndPriceSection/QtySelectSection.js
@@ -2,25 +2,45 @@ import React from 'react'
 import { Grid, Typography } from '@mui/material'
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Add as AddIcon, Remove as RemoveIcon } from '@mui/icons-material'
+
+const QtyButtons = [
+  {
+    type: 'add',
+    icon: <AddIcon />,
+  },
+  {
+    type: 'add',
+    icon: <RemoveIcon />,
+  },
+];
 
 export default function QtySelectSection() {
+
+
   return (
     <Grid container justifyContent={'flex-start'}>
-      <Grid item>
-        <Typography sx={{fontSize:'40px'}}>
+      <Grid item >
+        <Typography sx={{ fontSize: '40px' ,borderBottom:'1px solid blac'}}>
           02
         </Typography>
       </Grid>
-      <Grid item>
-        <IconButton aria-label="delete" size="large">
-          <DeleteIcon fontSize="inherit" />
-        </IconButton>
-      </Grid>
-      <Grid item>
-        <IconButton aria-label="delete" size="large">
-          <DeleteIcon fontSize="inherit" />
-        </IconButton>
-      </Grid>
+      {QtyButtons.map((val, key) => {
+        return (
+          <Grid item key={key} sx={{ ml: 2 ,mt:1}}>
+            <IconButton
+              sx={{
+                border: '1px solid gray',
+                borderRadius: '10px',
+
+              }}
+              aria-label="delete" size="large">
+              {val.icon}
+            </IconButton>
+          </Grid>
+        );
+      })
+      }
     </Grid>
   )
 }
