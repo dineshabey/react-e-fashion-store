bd233537238b9f359e3e7078bd8f614f7b99e21a
Dinesh <dinabeysinghe@gmail.com>
2023-11-06 22:01:20 +0530

    add all layouts from one layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import HomeLayuouts from './layouts/home/HomeLayouts';
-// import AllItemsLayouts from './layouts/allItem/AllItemsLayouts';
-import ProductDetailsLayouts from './layouts/productsDetails/ProductDetailsLayouts';
+import { Home, Details, AllItems } from './layouts/layouts';
 
 function App() {
   return (
     <div className='App'>
-      {/* <HomeLayuouts/> */}
-      {/* <AllItemsLayouts/> */}
-      <ProductDetailsLayouts/>
+      {/* <Home /> */}
+      <AllItems />
+      {/* <Details /> */}
     </div>
   );
 }
---
+++ src/layouts/layouts.js
@@ -0,0 +1,3 @@
+export { default as Home } from './home/HomeLayouts';
+export { default as AllItems } from './allItem/AllItemsLayouts';
+export { default as Details } from './productsDetails/ProductDetailsLayouts';
