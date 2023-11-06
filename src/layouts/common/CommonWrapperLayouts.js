7511c3ad3eaad0697a418f987743d3e65be6cb02
Dinesh <dinabeysinghe@gmail.com>
2023-11-06 22:33:34 +0530

    add for homepage 0 padding

--- src/layouts/common/CommonWrapperLayouts.js
+++ src/layouts/common/CommonWrapperLayouts.js
@@ -2,13 +2,11 @@ import React from 'react'
 import HeaderLayouts from './HeaderLayouts'
 import FooterLayouts from './FooterLayouts'
 
-export default function CommonWrapperLayouts({ children }) {
+export default function CommonWrapperLayouts({ children, isFromHome }) {
     return (
         <div>
             <HeaderLayouts />
-            <div style={{padding:'0 8%'}}>
-                {children}
-            </div>
+            <div style={{ padding: isFromHome ? '0' : '0 8%' }}> {children} </div>
             <FooterLayouts />
         </div>
     )
