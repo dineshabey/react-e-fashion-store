5e70f9c052a3a850347b428ad6bb3573318ac8f7
Dinesh <dinabeysinghe@gmail.com>
2023-11-13 12:31:50 +0530

    add cart page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Home, Details, AllItems , NotFound } from './layouts/layouts';
+import { Home, Details, AllItems, NotFound, Cart } from './layouts/layouts';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/all' element={<AllItems/>} />
-        <Route path='/details' element={<Details/>} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/all' element={<AllItems />} />
+        <Route path='/details' element={<Details />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
 
     </div>
