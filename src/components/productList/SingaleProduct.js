94866a895e1cdf27ff5f2b93973a04a42fd0a4e7
Dinesh <dinabeysinghe@gmail.com>
2023-11-20 08:29:02 +0530

    add link according to singale reponsibility princepale

--- src/components/productList/SingaleProduct.js
+++ src/components/productList/SingaleProduct.js
@@ -4,25 +4,29 @@ import ProductPrice from './singaleProduct/ProductPrice'
 import ProductSize from './singaleProduct/ProductSize'
 import ProductName from './singaleProduct/ProductName'
 import { Box } from '@mui/material'
+// import { Link } from 'react-router-dom'
 
 
 
 export default function SingaleProduct({ name, price, isAvailabelSize, type, images }) {
     return (
-        <Box sx={{
-            p: 3,
-            '&:hover': {
-                cursor: 'pointer',
-            }
-        }}
-        >
-            <ProductImage image={images} />
-            <Box sx={{ mt: 2 }}>
-                <ProductName name={name} type={type} />
-            </Box>
-            <ProductPrice price={price} />
-            <ProductSize isAvailabelSize={isAvailabelSize} />
+        // <Link to={'/details'} style={{ textDecoration: 'none', color: 'black' }}>
 
-        </Box>
+            <Box sx={{
+                p: 3,
+                '&:hover': {
+                    cursor: 'pointer',
+                }
+            }}
+            >
+                <ProductImage image={images} />
+                <Box sx={{ mt: 2 }}>
+                    <ProductName name={name} type={type} />
+                </Box>
+                <ProductPrice price={price} />
+                <ProductSize isAvailabelSize={isAvailabelSize} />
+
+            </Box>
+        // </Link>
     )
 }
--- src/layouts/allItem/innerLayouts/ProductLists.js
+++ src/layouts/allItem/innerLayouts/ProductLists.js
@@ -1,23 +1,27 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import SingaleProduct from '../../../components/productList/SingaleProduct';
+import { Link } from 'react-router-dom';
 
 export default function ProductLists({ products }) {
     return (
         <Grid container spacing={1} >
             {products
-            .map((val, key) => {
-                return <Grid key={key} item xs={12} sm={6} md={4} lg={3}>
-                        <SingaleProduct
-                            name={val.productName}
-                            price={val.price}
-                            isAvailabelSize={val.size}
-                            type={val.productType}
-                            images={[val.images]} 
-                            />
-                </Grid>
+                .map((val, key) => {
+                    return <Grid key={key} item xs={12} sm={6} md={4} lg={3}>
+                        <Link to={'/details'} style={{ textDecoration: 'none', color: 'black' }}>
 
-            })}
+                            <SingaleProduct
+                                name={val.productName}
+                                price={val.price}
+                                isAvailabelSize={val.size}
+                                type={val.productType}
+                                images={[val.images]}
+                            />
+                        </Link>
+                    </Grid>
+
+                })}
         </Grid>
     )
 }
