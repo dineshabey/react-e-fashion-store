c0def998ea6519f290f5c4d5875419e88b2c8660
Dinesh <dinabeysinghe@gmail.com>
2023-12-03 23:50:45 +0530

    cart store to cart redux setup done

--- src/layouts/cart/Cart.js
+++ src/layouts/cart/Cart.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import CommonWrapperLayouts from '../common/CommonWrapperLayouts'
+import { useSelector } from 'react-redux'
 
 export default function Cart() {
+    const cartReducer = useSelector(store => store.cartReducer);
+    console.log(cartReducer);
     return (
         <CommonWrapperLayouts>
             <h2>
