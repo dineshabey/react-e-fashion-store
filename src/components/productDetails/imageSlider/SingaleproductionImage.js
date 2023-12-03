3e0609d9bd49dad7e0255456fac254605a0594eb
Dinesh <dinabeysinghe@gmail.com>
2023-12-03 19:09:02 +0530

    uninstal react-image-magnify bcz package conflit

--- src/components/productDetails/imageSlider/SingaleproductionImage.js
+++ src/components/productDetails/imageSlider/SingaleproductionImage.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import ReactImageMagnify from 'react-image-magnify';
+// import ReactImageMagnify from 'react-image-magnify';
 
 export default function SingaleproductionImage({ watchImg300, watchImg1200 }) {
     return (
-        <ReactImageMagnify
-            imageStyle={{ borderRadius: '20px 0 0 20px' }}
-            enlargedImageContainerStyle={{ borderRadius: '20px 0 0 20px' }}
-            enlargedImagePosition='over'
-            {...{
-                smallImage: {
-                    alt: 'Wristwatch by Ted Baker London',
-                    // isFluidWidth: true,
-                    src: watchImg300,
-                    height: 450,
-                    width: 450,
-                },
-                largeImage: {
-                    src: watchImg1200,
-                    width: 700,
-                    height: 1600
-                },
-            }} />
+        <div></div>
+        // <ReactImageMagnify
+        //     imageStyle={{ borderRadius: '20px 0 0 20px' }}
+        //     enlargedImageContainerStyle={{ borderRadius: '20px 0 0 20px' }}
+        //     enlargedImagePosition='over'
+        //     {...{
+        //         smallImage: {
+        //             alt: 'Wristwatch by Ted Baker London',
+        //             // isFluidWidth: true,
+        //             src: watchImg300,
+        //             height: 450,
+        //             width: 450,
+        //         },
+        //         largeImage: {
+        //             src: watchImg1200,
+        //             width: 700,
+        //             height: 1600
+        //         },
+        //     }} />
     )
 }
